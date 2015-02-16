0a3414db7018f716423a0e0c1add43d5a9f7120b
barisser <barisser@gmail.com>
2015-02-16 09:55:15 -0800

    fixing typo

--- builddb.py
+++ builddb.py
@@ -5,7 +5,7 @@ while True:
     last_block_meta = db.dbexecute("select * from meta;", True)
     last_block_meta = last_block_meta[0][0]
 
-    last_block_node = bitcoind.connect("getblockcount". [])
+    last_block_node = bitcoind.connect("getblockcount", [])
 
     if last_block_meta < last_block_node-100:
         bitcoind.save_next_blocks(100)
